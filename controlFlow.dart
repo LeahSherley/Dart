cc93001f123aecd0dd4fdb5f99d1a7dbdd502aa7
Leah Sherley <lsherley90@gmail.com>
2024-03-08 09:03:13 +0300

    Added control flow

---
+++ controlFlow.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  print("Enter your favourite number:");
+  int? n = int.parse(stdin.readLineSync()!);
+
+  if (n > 10) {
+    print("$n is greater than 10");
+  } else if (n < 10) {
+    print("$n is less than ten");
+  } else {
+    print("$n is equal to ten");
+  }
+}
+
+
